5578636f270d76b1adfe9e0da723fe6dad31e9ed
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-01-20 11:18:18 +0100

    Add param to reset to another state

--- hooks/useForm/useForm.js
+++ hooks/useForm/useForm.js
@@ -6,8 +6,8 @@ export const useForm = (initialState = {}) => {
 
     const [values, setValues] = useState(initialState);
 
-    const reset = () => {
-        setValues(initialState);
+    const reset = ( newFormState = initialState ) => {
+        setValues( newFormState );
     }
 
     const handleInputChange = ({
